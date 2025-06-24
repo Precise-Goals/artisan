47e0999fd886513f0d6c7c385d320f3220d49878
Precise Goals <elitesarthakamr@gmail.com>
2025-06-24 14:10:17 +0530

    smooth scroll shitter

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-        <SmoothScroll>{children}</SmoothScroll>
+        {children}
       </body>
     </html>
   );
